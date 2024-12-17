313d1b51bf6514aa066c2456ac6c54eedcf65e78
Yogeshwar198 <yogeshwaredu198@gmail.com>
2024-12-17 13:02:20 +0530

    URL updated in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand"
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.evn.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 axios.defaults.withCredentials = true;
 
@@ -81,16 +81,16 @@ export const useAuthStore = create((set) => ({
         }
     },
     resetPassword: async (token, password) => {
-		set({ isLoading: true, error: null });
-		try {
-			const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
-			set({ message: response.data.message, isLoading: false });
-		} catch (error) {
-			set({
-				isLoading: false,
-				error: error.response.data.message || "Error resetting password",
-			});
-			throw error;
-		}
-	},
+        set({ isLoading: true, error: null });
+        try {
+            const response = await axios.post(`${API_URL}/reset-password/${token}`, { password });
+            set({ message: response.data.message, isLoading: false });
+        } catch (error) {
+            set({
+                isLoading: false,
+                error: error.response.data.message || "Error resetting password",
+            });
+            throw error;
+        }
+    },
 }))
